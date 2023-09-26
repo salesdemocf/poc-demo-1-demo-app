c8a144f8d38209c25848562ff57b115509d36132
Dustin Van Buskirk <dev@vanbuskirk.me>
2023-09-26 15:36:28 -0700

    update mice to birds

--- vote/app.py
+++ vote/app.py
@@ -7,7 +7,7 @@ import json
 import logging
 
 option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Mice")
+option_b = os.getenv('OPTION_B', "Birds")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
