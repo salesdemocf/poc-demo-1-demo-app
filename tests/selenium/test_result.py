658ffc12e6d885f2d629c5e5640c72a83c5e085a
Dustin Van Buskirk <dev@vanbuskirk.me>
2023-09-27 12:31:07 -0700

    update snakes to cats

--- tests/selenium/test_result.py
+++ tests/selenium/test_result.py
@@ -31,7 +31,7 @@ def browser():
 
 def test_confirm_result_title(browser):
     browser.get("http://{}".format(result_endpoint))
-    assert "Mice vs Snakes -- Result" in browser.title
+    assert "Mice vs Cats -- Result" in browser.title
 
 
 def test_confirm_result(browser):
