4b53f55ca5c66c82c8e725a68f3cf7c12559e2c3
Dustin Van Buskirk <dev@vanbuskirk.me>
2023-09-26 21:16:29 -0700

    update hamsters to birds

--- tests/selenium/test_result.py
+++ tests/selenium/test_result.py
@@ -31,7 +31,7 @@ def browser():
 
 def test_confirm_result_title(browser):
     browser.get("http://{}".format(result_endpoint))
-    assert "Cats vs Hamsters -- Result" in browser.title
+    assert "Cats vs Birds -- Result" in browser.title
 
 
 def test_confirm_result(browser):
