395e2d507e752560fe8789ac68ee95adfea0a29b
Dustin Van Buskirk <dev@vanbuskirk.me>
2023-09-28 12:23:39 -0700

    update vote from dogs to cats

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Dogs")
+option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Birds")
 hostname = socket.gethostname()
 
