a941e01052483b379c8909d04e4d27365facf1a5
Dustin Van Buskirk <dev@vanbuskirk.me>
2023-11-28 21:24:46 -0800

    update vote from dogs to rats

--- vote/app.py
+++ vote/app.py
@@ -7,7 +7,7 @@ import json
 import logging
 
 option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_b = os.getenv('OPTION_B', "Rats")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
