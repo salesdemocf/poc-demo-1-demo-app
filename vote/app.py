2b8adb146f3d18838b9d12b6c37b8a6083b874f7
Dustin Van Buskirk <dev@vanbuskirk.me>
2023-12-05 14:39:47 -0800

    change to ninjas

--- vote/app.py
+++ vote/app.py
@@ -7,7 +7,7 @@ import json
 import logging
 
 option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_b = os.getenv('OPTION_B', "Ninjas")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
