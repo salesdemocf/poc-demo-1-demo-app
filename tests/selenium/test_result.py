965045f88791fab0d0d98e415d156a2421961b6c
Dustin Van Buskirk <dev@vanbuskirk.me>
2023-12-20 09:18:39 -0800

    orcs vs hobbits

--- tests/selenium/test_result.py
+++ tests/selenium/test_result.py
@@ -31,7 +31,7 @@ def browser():
 
 def test_confirm_result_title(browser):
     browser.get("http://{}".format(result_endpoint))
-    assert "Hobbits vs Elves -- Result" in browser.title
+    assert "Orcs vs Hobbits -- Result" in browser.title
 
 
 def test_confirm_result(browser):
