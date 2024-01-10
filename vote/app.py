2a469ef2d6be0741dc5e117e6ce51cd6d2a59970
Dustin Van Buskirk <dev@vanbuskirk.me>
2024-01-10 08:55:15 -0800

    update vote and bump minor

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Orcs")
+option_a = os.getenv('OPTION_A', "Goblins")
 option_b = os.getenv('OPTION_B', "Wizards")
 hostname = socket.gethostname()
 
