a93b6d45ea1af73946b64c56dbf560f93bbedc42
Dustin Van Buskirk <dev@vanbuskirk.me>
2024-01-26 13:54:51 -0800

    update vote to warriors

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Goblins")
+option_a = os.getenv('OPTION_A', "Warriors")
 option_b = os.getenv('OPTION_B', "Orcs")
 hostname = socket.gethostname()
 
