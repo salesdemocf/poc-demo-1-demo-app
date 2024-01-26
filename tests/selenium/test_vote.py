d10967b18aa3f6882660726dfda795dd0f3d002e
Dustin Van Buskirk <dev@vanbuskirk.me>
2024-01-26 12:53:50 -0800

    update vote to goblins

--- tests/selenium/test_vote.py
+++ tests/selenium/test_vote.py
@@ -35,7 +35,7 @@ def browser():
 
 def test_confirm_vote_title(browser):
     browser.get("http://{}".format(vote_endpoint))
-    option_a = "Witches"
+    option_a = "Goblins"
     option_b = "Orcs"
     assert "{} vs {}!".format(option_a, option_b) in browser.title
 
