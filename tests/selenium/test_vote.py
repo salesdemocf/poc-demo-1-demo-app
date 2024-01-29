e0bf37b51a8a013fa4aa7c9b5b6ef5c81319d62a
Dustin Van Buskirk <dev@vanbuskirk.me>
2024-01-29 07:41:54 -0800

    update voting app to trolls

--- tests/selenium/test_vote.py
+++ tests/selenium/test_vote.py
@@ -36,7 +36,7 @@ def browser():
 def test_confirm_vote_title(browser):
     browser.get("http://{}".format(vote_endpoint))
     option_a = "Warriors"
-    option_b = "Orcs"
+    option_b = "Trolls"
     assert "{} vs {}!".format(option_a, option_b) in browser.title
 
 
