f2aadd2ef437e2668146afeeed7715f82973da16
Dustin Van Buskirk <dev@vanbuskirk.me>
2024-01-29 07:41:54 -0800

    update voting app to trolls

--- vote/app.py
+++ vote/app.py
@@ -7,7 +7,7 @@ import json
 import logging
 
 option_a = os.getenv('OPTION_A', "Warriors")
-option_b = os.getenv('OPTION_B', "Orcs")
+option_b = os.getenv('OPTION_B', "Trolls")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
