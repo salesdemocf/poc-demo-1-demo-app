8dc44c22533b8eef39f462b4b23d752d66e57370
Dustin Van Buskirk <dev@vanbuskirk.me>
2024-01-29 17:35:53 -0800

    updating result application to warlocks

--- tests/selenium/test_result.py
+++ tests/selenium/test_result.py
@@ -31,7 +31,7 @@ def browser():
 
 def test_confirm_result_title(browser):
     browser.get("http://{}".format(result_endpoint))
-    assert "Wizards vs Goblins -- Result" in browser.title
+    assert "Warlocks vs Goblins -- Result" in browser.title
 
 
 def test_confirm_result(browser):
